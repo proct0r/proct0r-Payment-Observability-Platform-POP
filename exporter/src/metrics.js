ac581e0d69c97ae7c2ccdb989388a73a5152f680
SOLOMON <62173545+proct0r@users.noreply.github.com>
2026-07-16 11:46:04 +0100

    Integrate Prometheus monitoring pipeline

--- exporter/src/metrics.js
+++ exporter/src/metrics.js
@@ -1,37 +1,39 @@
 import client from "prom-client";
-
-
 client.collectDefaultMetrics();
 
-
 export const transactionVolume = new client.Counter({
-
     name:
     "payment_transaction_volume_total",
-
     help:
-    "Total payment transaction volume"
-
+    "Total payment transaction volume processed"
 });
 
-
-export const successfulTransactions = new client.Counter({
-
+export const approvedTransactions = new client.Counter({
     name:
-    "payment_successful_transactions_total",
-
+    "payment_approved_transactions_total",
     help:
-    "Total successful transactions"
-
+    "Total approved payment transactions"
 });
 
-
-export const failedTransactions = new client.Counter({
-
+export const customerDeclines = new client.Counter({
     name:
-    "payment_failed_transactions_total",
-
+    "payment_customer_declines_total",
     help:
-    "Total failed transactions"
-
+    "Transactions declined due to customer related reasons"
 });
+
+export const systemDeclines = new client.Counter({
+    name:
+    "payment_system_declines_total",
+    help:
+    "Transactions declined due to system failures"
+});
+
+export const exporterHealth = new client.Gauge({
+    name:
+    "payment_exporter_health",
+    help:
+    "Exporter availability status"
+});
+
+exporterHealth.set(1);
